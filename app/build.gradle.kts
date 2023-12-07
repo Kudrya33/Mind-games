6c0a68724d62aef8b53b7a42243fe3e3ba39818c
Павел Кудря <Skelger@mail.ru>
2023-12-07 18:02:31 +0300

    add application plugin on gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -17,3 +18,9 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
