3679a90c76f110264e17f18641da232f404cd599
Павел Кудря <Skelger@mail.ru>
2023-12-08 23:06:48 +0300

    add new class Cli.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+    public static void greetsThePlayer() {
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.next();
+        sc.close();
+        System.out.println("Hello, " + userName + "!");
+    }
+}
