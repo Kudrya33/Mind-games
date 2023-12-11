ae947c2f58ec897e395279d2bd4634b3b2b9ce04
Павел Кудря <Skelger@mail.ru>
2023-12-12 02:04:54 +0300

    update list of games

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,27 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+
+        Scanner scanNumberGame = new Scanner(System.in);
+        int gameNumberSelection = scanNumberGame.nextInt();
+
+        System.out.println("Your choice: " + gameNumberSelection);
         System.out.println("Welcome to the Brain Games!");
-        Cli.greetsThePlayer();
+
+        switch (gameNumberSelection) {
+            case 0:
+                break;
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                Cli.greetsThePlayer();
+            case 2:
+                Even.startsTheGameEven();
+            default:
+                break;
+        }
     }
 }
