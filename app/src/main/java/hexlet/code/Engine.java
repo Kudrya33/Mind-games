0b761995f39d664b67cd1a0e7913e7ae58c0d5a2
Павел Кудря <Skelger@mail.ru>
2023-12-13 23:40:41 +0300

    add class Engine

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static void greetsThePlayer() {
+        System.out.println("May I have your name?");
+
+        Scanner scanUserName = new Scanner(System.in);
+        String userName = scanUserName.next();
+
+        System.out.println("Hello, " + userName + "!");
+    }
+
+}
