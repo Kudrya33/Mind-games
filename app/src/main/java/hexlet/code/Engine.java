a009af126f528b177f1a816fd99b8ca96af72ead
Павел Кудря <Skelger@mail.ru>
2023-12-16 23:54:17 +0300

    update work engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,5 +1,33 @@
 package hexlet.code;
 
-public class Engine {
+import java.util.Scanner;
 
+public class Engine {
+    public static int launchEngine(String userName, String rules, String question, String correctAnswer, int i, int countCorrectAnswer) {
+        if (i == 0) {
+            System.out.println(rules);
+        }
+        if (i < 3) {
+            System.out.println(question);
+
+            Scanner scanAnswer = new Scanner(System.in);
+            String playerAnswer = scanAnswer.nextLine();
+
+            System.out.println("Your answer: " + playerAnswer);
+
+            if (playerAnswer.equalsIgnoreCase(correctAnswer)) {
+                System.out.println("Correct!");
+                countCorrectAnswer++;
+            }
+            if (playerAnswer.equalsIgnoreCase(correctAnswer) && countCorrectAnswer == 3) {
+                System.out.println("Congratulations, " + userName + "!");
+            }
+            if (!playerAnswer.equalsIgnoreCase(correctAnswer)) {
+                System.out.println("'" + playerAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+                System.out.println("Let's try again, " + userName + "!");
+                countCorrectAnswer = -1;
+            }
+        }
+        return countCorrectAnswer;
+    }
 }
