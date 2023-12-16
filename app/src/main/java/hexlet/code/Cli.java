7edea9f1cb2a3939cad4dad1834f15d27910d1f7
Павел Кудря <Skelger@mail.ru>
2023-12-16 23:53:35 +0300

    add interaction with game

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,12 +1,13 @@
 package hexlet.code;
 import java.util.Scanner;
 public class Cli {
-    public static void greetsThePlayer() {
+    public static String greetsThePlayer() {
         System.out.println("May I have your name?");
 
         Scanner scanUserName = new Scanner(System.in);
         String userName = scanUserName.next();
 
         System.out.println("Hello, " + userName + "!");
+        return userName;
     }
 }
