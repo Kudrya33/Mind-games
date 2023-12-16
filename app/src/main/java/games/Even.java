10965aada128f1cf2c17dbfd5eaf93ddd695615c
Павел Кудря <Skelger@mail.ru>
2023-12-16 23:52:24 +0300

    added interaction with the engine

--- app/src/main/java/games/Even.java
+++ app/src/main/java/games/Even.java
@@ -1,44 +1,33 @@
 package games;
 
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class Even {
     public static void startsTheGameEven() {
-        System.out.println("May I have your name?");
+        String userName = Cli.greetsThePlayer();
 
-        Scanner scanUserName = new Scanner(System.in);
-        String userName = scanUserName.next();
-
-        System.out.println("Hello, " + userName + "!");
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
+        String rulesOfTheGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         int countCorrectAnswer = 0;
-        String correctAnswer;
+
+        String correctAnswer = "";
         Random random = new Random();
 
         for (int i = 0; i < 3; i++) {
-            int evenNumber = random.nextInt(100);
-            System.out.println("Question: " + evenNumber);
-
-            Scanner scanAnswerEven = new Scanner(System.in);
-            String answerForEven = scanAnswerEven.next();
-
-            if (evenNumber % 2 == 0 && answerForEven.equalsIgnoreCase("yes")) {
-                System.out.println("Your answer: 'Yes'\nCorrect!");
-                countCorrectAnswer++;
-            }
-            if (evenNumber % 2 != 0 && answerForEven.equalsIgnoreCase("no")) {
-                System.out.println("Your answer: 'No'\nCorrect!");
-                countCorrectAnswer++;
-            } else if (evenNumber % 2 == 0 && answerForEven.equalsIgnoreCase("no") || evenNumber % 2 != 0 && answerForEven.equalsIgnoreCase("yes")) {
-                correctAnswer = answerForEven.equalsIgnoreCase("Yes") ? "no" : "yes";
-                System.out.println("'" + answerForEven + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.\nLet's try again, " + userName + "!");
+            if (countCorrectAnswer == -1) {
                 break;
             }
-        }
-        if (countCorrectAnswer == 3) {
-            System.out.println("Congratulations, " + userName + "!");
+            int evenNumber = random.nextInt(100);
+            if (evenNumber%2 == 0) {
+                correctAnswer = "yes";
+            }
+            if (evenNumber%2 != 0) {
+                correctAnswer = "no";
+            }
+            String question = "Question: " + evenNumber;
+            countCorrectAnswer = Engine.launchEngine(userName, rulesOfTheGame, question, correctAnswer, i, countCorrectAnswer);
         }
     }
 }
