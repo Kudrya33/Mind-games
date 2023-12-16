56073d79219407c7ca500e9fea541b1620b2808a
Павел Кудря <Skelger@mail.ru>
2023-12-16 23:51:44 +0300

    added interaction with the engine

--- app/src/main/java/games/Calc.java
+++ app/src/main/java/games/Calc.java
@@ -1,53 +1,39 @@
 package games;
 
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class Calc {
     public static void startsTheGameCalc() {
-        System.out.println("May I have your name?");
+        String userName = Cli.greetsThePlayer();
 
-        Scanner scanUserName = new Scanner(System.in);
-        String userName = scanUserName.next();
-
-        System.out.println("Hello, " + userName + "!");
-        System.out.println("What is the result of the expression?");
-
+        String rulesOfTheGame = "What is the result of the expression?";
         int countCorrectAnswer = 0;
+
         Random random = new Random();
 
         for (int i = 0; i < 3; i++) {
+            if (countCorrectAnswer == -1) {
+                break;
+            }
             int number1 = random.nextInt(100);
             int number2 = random.nextInt(100);
 
             String[] operands = {"+", "-", "*"};
             int selectedOperand = random.nextInt(3);
             String operand = operands[selectedOperand];
-            int answerCalc = switch (operand) {
+            int correctAnswer = switch (operand) {
                 case "+" -> number1 + number2;
                 case "-" -> number1 - number2;
                 case "*" -> number1 * number2;
                 default -> 0;
             };
+            String correctAnswerToString = Integer.toString(correctAnswer);
 
-            System.out.println("Question: " + number1 + " " + operands[selectedOperand] + " " + number2);
-
-            Scanner scanAnswer = new Scanner(System.in);
-            int playerAnswer = scanAnswer.nextInt();
-
-            if (answerCalc == playerAnswer) {
-                countCorrectAnswer++;
-                System.out.println("Your answer: " + playerAnswer + "\nCorrect!");
-            }
-            if (answerCalc != playerAnswer) {
-                System.out.println("Your answer: " + playerAnswer);
-                System.out.println("'" + playerAnswer + "' " + "is wrong answer ;(. Correct answer was " + "'" + answerCalc + "'.");
-                System.out.println("Let's try again, " + userName + "!");
-                break;
-            }
-        }
-        if (countCorrectAnswer == 3) {
-            System.out.println("Congratulations, " + userName + "!");
+            String question = "Question: " + number1 + " " + operands[selectedOperand] + " " + number2;
+            countCorrectAnswer = Engine.launchEngine(userName, rulesOfTheGame, question, correctAnswerToString, i, countCorrectAnswer);
         }
     }
 }
