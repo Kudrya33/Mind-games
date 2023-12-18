cfaad9ad985dde318ed2fd9ae8ffaecd78db7f79
Павел Кудря <Skelger@mail.ru>
2023-12-18 23:30:35 +0300

    add choice new game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,12 +2,13 @@ package hexlet.code;
 
 import games.Calc;
 import games.Even;
+import games.GCD;
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n0 - Exit");
 
         Scanner scanNumberGame = new Scanner(System.in);
         int gameNumberSelection = scanNumberGame.nextInt();
@@ -24,6 +25,9 @@ public class App {
                 break;
             case 3:
                 Calc.startsTheGameCalc();
+                break;
+            case 4:
+                GCD.startsTheGameGCD();
                 break;
             default:
                 break;
