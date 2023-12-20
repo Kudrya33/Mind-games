86b683bfa532ca5172f9247053a32ba4cc3e727d
Павел Кудря <Skelger@mail.ru>
2023-12-20 20:41:00 +0300

    add new class Prime

---
+++ app/src/main/java/games/Prime.java
@@ -0,0 +1,41 @@
+package games;
+
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+public class Prime {
+    public static void startsTheGamePrime() {
+        String userName = Cli.greetsThePlayer();
+
+        String rulesOfTheGame = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+
+        int countCorrectAnswer = 0;
+
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            if (countCorrectAnswer == -1) {
+                break;
+            }
+            int number = random.nextInt(100);
+            String correctAnswer = "";
+            if (number < 2) {
+                correctAnswer = "no";
+            }
+            if (number >= 2) {
+                for(int j = 2; j <= number / 2; j++) {
+                    if (number % j == 0) {
+                        correctAnswer = "no";
+                        break;
+                    } else {
+                        correctAnswer = "yes";
+                    }
+                }
+            }
+            String question = "Question: " + number;
+            countCorrectAnswer = Engine.launchEngine(userName, rulesOfTheGame, question, correctAnswer, i, countCorrectAnswer);
+        }
+    }
+}
