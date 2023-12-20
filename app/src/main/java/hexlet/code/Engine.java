283eb015efc5da26589a7c1369adec3a0496f879
Павел Кудря <Skelger@mail.ru>
2023-12-20 23:44:38 +0300

    fix 002

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Engine {
     public static int upEngine(String userName, String rules, String question,
                                String correctAnswer, int i, int countAnswer) {
-
-        if (i == 0) {
+        int finish = 3;
+        int numberRound;
+        numberRound = i;
+        if (numberRound == 0) {
             System.out.println(rules);
         }
-        if (i < 3) {
+        if (numberRound < 3) {
             System.out.println(question);
 
             Scanner scanAnswer = new Scanner(System.in);
@@ -21,12 +23,11 @@ public class Engine {
                 System.out.println("Correct!");
                 countAnswer++;
             }
-            if (playerAnswer.equalsIgnoreCase(correctAnswer) && countAnswer == i) {
+            if (playerAnswer.equalsIgnoreCase(correctAnswer) && countAnswer == finish) {
                 System.out.println("Congratulations, " + userName + "!");
             }
             if (!playerAnswer.equalsIgnoreCase(correctAnswer)) {
-                System.out.println("'" + playerAnswer + "' is wrong answer ;(. "
-                        + "Correct answer was '" + correctAnswer + "'.");
+                System.out.println("'" + playerAnswer + "' is wrong answer ;(. " + "Correct answer was '" + correctAnswer + "'.");
                 System.out.println("Let's try again, " + userName + "!");
                 countAnswer = -1;
             }
